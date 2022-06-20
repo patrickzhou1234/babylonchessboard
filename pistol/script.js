393f2ac1b87204b25f3a64bf5fe6a28e6045966b
Patrick <83720283+patrickzhou1234@users.noreply.github.com>
2022-06-20 14:02:57 -0700

    Create script.js

---
+++ pistol/script.js
@@ -0,0 +1,42 @@
+const canvas = document.getElementById("babcanv"); // Get the canvas element
+const engine = new BABYLON.Engine(canvas, true);
+var createScene = function (canvas, engine) {
+    // This creates a basic Babylon Scene object (non-mesh)
+    var scene = new BABYLON.Scene(engine);
+
+    // This creates and positions a free camera (non-mesh)
+    camera = new BABYLON.ArcRotateCamera("camera", -10, -10, 100, new BABYLON.Vector3.Zero(), scene);
+
+    // This targets the camera to scene origin
+    camera.setTarget(BABYLON.Vector3.Zero());
+
+    // This attaches the camera to the canvas
+    camera.attachControl(canvas, true);
+
+    // This creates a light, aiming 0,1,0 - to the sky (non-mesh)
+    var light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 1, 0), scene);
+
+    // Default intensity is 1. Let's dim the light a small amount
+    light.intensity = 0.7;
+
+    // Our built-in 'sphere' shape.
+    var building = BABYLON.SceneLoader.Append("", "guncompiled.babylon", scene, function (meshes) {
+      camera.target = meshes[0];
+    });
+
+    return scene;
+};
+
+function move(event) {
+  
+}
+
+const scene = createScene();
+
+engine.runRenderLoop(function () {
+  scene.render();
+});
+
+window.addEventListener("resize", function () {
+  engine.resize();
+});
