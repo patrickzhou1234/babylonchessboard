dde35786731a6201000af79ad8b63cb5f2057949
Patrick <83720283+patrickzhou1234@users.noreply.github.com>
2022-06-20 08:27:09 -0700

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ var createScene = function (canvas, engine) {
     var scene = new BABYLON.Scene(engine);
 
     // This creates and positions a free camera (non-mesh)
-    camera = new BABYLON.ArcRotateCamera("camera", 0, 50, 0, new BABYLON.Vector3(0, 0, -10), scene);
+    camera = new BABYLON.ArcRotateCamera("camera", 0, 50, 100, new BABYLON.Vector3.Zero(), scene);
 
     // This targets the camera to scene origin
     camera.setTarget(BABYLON.Vector3.Zero());
@@ -17,7 +17,7 @@ var createScene = function (canvas, engine) {
     var light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 1, 0), scene);
 
     // Default intensity is 1. Let's dim the light a small amount
-    light.intensity = 5;
+    light.intensity = 0.7;
 
     // Our built-in 'sphere' shape.
     var building = BABYLON.SceneLoader.Append("", "Chess Scene.glb", scene, function (meshes) {
