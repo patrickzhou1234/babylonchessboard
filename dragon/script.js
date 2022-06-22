5369fb1f5433b072fe2ef09499ea7b0dde93b0cd
Patrick <83720283+patrickzhou1234@users.noreply.github.com>
2022-06-22 08:52:22 -0700

    Update script.js

--- dragon/script.js
+++ dragon/script.js
@@ -26,7 +26,7 @@ var createScene = function (canvas, engine) {
 
     // Our built-in 'sphere' shape.
     var building = BABYLON.SceneLoader.Append("", "dragon.glb", scene, function (meshes) {
-      meshes[0].scaling = new BABYLON.Vector3(5, 5, 5);
+      meshes[0].scaling = new BABYLON.Vector3(10, 10, 10);
       camera.target = meshes[0];
     });
 
